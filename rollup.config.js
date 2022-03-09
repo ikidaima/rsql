114ab1d5b4b4f51e9f2ff1035a52c03a74e51deb
Dmitriy Bulychev <dbulychev@ubic.tech>
2022-03-09 18:03:45 +0300

    update rollup config

--- rollup.config.js
+++ rollup.config.js
@@ -1,15 +1,39 @@
+import resolve from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
+import { babel } from '@rollup/plugin-babel';
 import { terser } from 'rollup-plugin-terser';
+import pkg from './package.json';
 
-export default {
-  input: 'src/index.js',
-  output: [{
-    file: 'dist/main.js',
-    format: 'cjs',
-    plugins: [terser()],
-  },
+export default [
   {
-    file: 'dist/module.js',
-    format: 'es',
-    plugins: [terser()],
-  }],
-};
+    input: 'src/index.js',
+    output: {
+      name: 'oauthClientService',
+      file: pkg.browser,
+      format: 'umd',
+    },
+    plugins: [
+      resolve(),
+      commonjs(),
+      babel({
+        exclude: ['node_modules/**'],
+      }),
+      terser(),
+    ],
+  },
+
+  {
+    input: 'src/index.js',
+    external: [],
+    output: [
+      { file: pkg.main, format: 'cjs' },
+      { file: pkg.module, format: 'es' },
+    ],
+    plugins: [
+      babel({
+        exclude: ['node_modules/**'],
+      }),
+      terser(),
+    ],
+  },
+];
