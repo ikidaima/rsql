651ef5d12a033ce22ce94fee4184e2c12a61cb48
Dmitriy Bulychev <dbulychev@ubic.tech>
2022-03-09 16:43:36 +0300

    rollup.config.js update config

--- rollup.config.js
+++ rollup.config.js
@@ -1,7 +1,15 @@
+import { terser } from 'rollup-plugin-terser';
+
 export default {
   input: 'src/index.js',
-  output: {
-    file: 'dist/bundle.js',
-    format: 'cjs'
-  }
+  output: [{
+    file: 'dist/main.js',
+    format: 'cjs',
+    plugins: [terser()],
+  },
+  {
+    file: 'dist/module.js',
+    format: 'es',
+    plugins: [terser()],
+  }],
 };
